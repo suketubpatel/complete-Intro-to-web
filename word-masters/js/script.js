a28365a0cf21c800cc28f4b919605f203f69e2a3
Suketu Patel <suketu@usa.net>
2023-11-28 23:09:30 +1100

    Matched word is working with making it green

--- word-masters/js/script.js
+++ word-masters/js/script.js
@@ -1,3 +1,10 @@
+const rows = 6, columns = 5;
+const logo = document.querySelector(".hidden");         // spining object div
+const btn = document.querySelector(".btn-container");      // button to play again
+
+
+async function init() {           // read key strok from keyboard on body element
+    
 let word;
 let currentLetter = 0;
 let keyStrock = 0;
@@ -5,122 +12,130 @@ let lastRowLetter = 0;
 let currentWord = "";
 let letterArray = [];
 let displayLetter;
+let currentRowNum = 1;
+let currentRow;
 
-const rows = 6, columns = 5;
+    document
+    .querySelector("body")
+        .addEventListener("keydown", function(event) {
+            keyPress(event.key);
+        });
 
-const logo = document.querySelector(".hidden");         // spining object div
-
-
-function keyPress(value){
-    
-    let isLetter = checkLetter(value);
-
-    //console.log(keyStrock);
-    if (keyStrock == 5) {                       // if at the end of each row
-        
-        if (value == "Enter") {                 // if user has pressed enter
-            console.log(currentLetter);
-            console.log(keyStrock);
-            if (matched = matchWord()) {        // match the word
-                lastRowLetter = currentLetter;
-                keyStrock = 0;
-            } else {
-                return;
-            }
-        } else if (value == "Backspace") {      // only backspace allowed
-            backSpace();
-        } else {
-            return;
-        }
-        
-    } else if (value == "Backspace" && currentLetter > lastRowLetter) {         // delete last letter
-        backSpace();
-    } else if (isLetter && currentLetter < 30) {                                // write letter
-        addLetter(value);
-    }
-}
-
-function matchWord(){
-    console.log(currentWord);
-    for (let i = 0; i < letterArray.length; i++) {
-        console.log(`${i}: ${letterArray[i]}`);
-        //let block = document.querySelector("#letter-" + i);
-        //block.style.backgroundColor = "green";
-    }
-    currentWord = "";
-    return true;
-}
-
-function backSpace() {
-    currentLetter--;
-    keyStrock--;
-    displayLetter  = document.querySelector(`#letter-${currentLetter}`);
-
-    displayLetter.innerText = "";
-    displayLetter.style.boxShadow = "2px 2px 4px #000000";
-    displayLetter.style.backgroundColor = "#FFFFFF";
-
-    currentWord = currentWord.substring(0, currentWord.length - 1);
-    letterArray.pop();
-}
-
-function addLetter(value) {
-    displayLetter  = document.querySelector(`#letter-${currentLetter}`);
-
-    displayLetter.innerText = value.toUpperCase();
-    displayLetter.style.boxShadow = "2px 2px 4px #000000 inset";
-    displayLetter.style.backgroundColor = "#F0F8FF";
-    currentLetter++;
-    keyStrock++;
-
-    currentWord += value;
-    letterArray.push(value);
-}
-
-function checkLetter(letter) {
-    return /^[a-zA-ZEnter]$/.test(letter);
-}
-
-function getWord() {                // Fetch the word from the API
     let word_of_the_day, word_of_the_day_random;
-    
     const word_of_the_day_url = "https://words.dev-apis.com/word-of-the-day";
     const word_of_the_day_random_url = "https://words.dev-apis.com/word-of-the-day?random=1";
 
-    async function fetchWord() {
-        // add hiddent log css here
         logo.style.visibility = "display";      // display spining object
 
         // fetch daily word
         const promise = await fetch(word_of_the_day_url);
         const promiseResponse = await promise.json();
         word_of_the_day = promiseResponse.word;
+        word = word_of_the_day;
+        console.log(word);
 
         /*
         // fetch daily word
         const promise = await fetch(word_of_the_day_random_url);
         const promiseResponse = await promise.json();
         word_of_the_day_random = promiseResponse.word;
-        console.log(word_of_the_day_random);
+        word = word_of_the_day_random;
+        console.log(word);
         */
 
         logo.style.visibility = "hidden";       // hide spining object
-        console.log(word_of_the_day);
+            
+
+    function keyPress(value){
+        
+        console.log(`current row num: ${currentRowNum}`);
+
+        let isLetter = checkLetter(value);
+
+        //console.log(keyStrock);
+        if (keyStrock == 5) {                       // if at the end of each row
+            
+            if (value == "Enter") {                 // if user has pressed enter
+                
+                console.log(currentWord);
+                console.log(`current row num: ${currentRowNum}`);
+                console.log(".row-" + currentRowNum + " > *");
+
+                currentRow = document.querySelector(".row-" + currentRowNum);     // css to current row
+
+                if (!matchWord()) {        // matched the word
+                    lastRowLetter = currentLetter;
+                    keyStrock = 0;
+                    currentWord = "";
+
+                    currentRowNum++;        // increase the number of current row
+                    return;
+                } else {
+                    
+                    // Colour the letters green to show this row is correct and matched
+                    if(currentRow.className.indexOf("row-" + currentRowNum) > -1) {
+                        
+                        for(let i = currentLetter - 1; i >= (currentLetter - 5); i--){
+                            let letter = document.querySelector("#letter-" + i);
+                            letter.style.backgroundColor = "green";
+                            letter.style.color = "white";
+                        }
+                    }
+                    btn.style.visibility = "visible";
+                    
+                    return;
+                }
+            } else if (value == "Backspace") {      // only backspace allowed
+                backSpace();
+            } else {
+                return;
+            }
+            
+        } else if (value == "Backspace" && currentLetter > lastRowLetter) {         // delete last letter
+            backSpace();
+        } else if (isLetter && currentLetter < 30) {                                // write letter
+            addLetter(value);
+        }
+        
     }
-    
-    fetchWord();        // Function call
-    return word_of_the_day;
-    //return word_of_the_day_random;
-}
 
-function init() {           // read key strok from keyboard on body element
-    document
-        .querySelector("body")
-            .addEventListener("keydown", function(event) {
-                keyPress(event.key);
-            });
+    function matchWord(){
+        if (currentWord == word){        // if matched return true
+            currentWord = "";
+            return true;
+        }
+        return false;                    // else return false
+    }
 
-    word = getWord();              // fetch the wrod from API
+    function backSpace() {
+        currentLetter--;
+        keyStrock--;
+        displayLetter  = document.querySelector(`#letter-${currentLetter}`);
+
+        displayLetter.innerText = "";
+        displayLetter.style.boxShadow = "2px 2px 4px #000000";
+        displayLetter.style.backgroundColor = "#FFFFFF";
+
+        currentWord = currentWord.substring(0, currentWord.length - 1);
+        letterArray.pop();
+    }
+
+    function addLetter(value) {
+        displayLetter  = document.querySelector(`#letter-${currentLetter}`);
+
+        displayLetter.innerText = value.toUpperCase();
+        displayLetter.style.boxShadow = "2px 2px 4px #000000 inset";
+        displayLetter.style.backgroundColor = "#F0F8FF";
+        currentLetter++;
+        keyStrock++;
+
+        currentWord += value;
+        letterArray.push(value);
+    }
+
+    function checkLetter(letter) {
+        return /^[a-zA-ZEnter]$/.test(letter);
+    }
 }
 
 init();                     // run javascrip
