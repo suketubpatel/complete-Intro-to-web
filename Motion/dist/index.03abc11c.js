ef9d9070cc1fa28da6c373f13b911cc78e3e7d31
Suketu Patel <suketu@usa.net>
2023-12-06 22:10:14 +1100

    Animation changed

---
+++ Motion/dist/index.03abc11c.js
@@ -0,0 +1,13 @@
+const ball = document.querySelector(".ball");
+popmotion.animate({
+    from: "0px",
+    to: "100px",
+    repeat: Infinity,
+    repeatType: "mirror",
+    type: "spring",
+    onUpdate (update) {
+        ball.style.left = update;
+    }
+});
+
+//# sourceMappingURL=index.03abc11c.js.map
